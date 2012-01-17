9a6743ba3459a0b8df64bdefd7b66d629a43faa2
Jacob Dunn <jakemdunn@gmail.com>
2012-01-17 12:36:42 -0600

    Added remaining dependencies, offloaded jquery to Google CDN.

---
+++ js/libs/jquery.cookie.js
@@ -0,0 +1,10 @@
+/**
+ * jQuery Cookie plugin
+ *
+ * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('l.5=4(a,b,c){6(u.C>1&&h(b)!=="[w x]"){c=l.y({},c);6(b===k||b===D){c.0=-1}6(q c.0===\'r\'){3 d=c.0,t=c.0=f A();t.B(t.O()+d)}b=h(b);2(n.5=[7(a),\'=\',c.i?b:j(b),c.0?\'; 0=\'+c.0.v():\'\',c.8?\'; 8=\'+c.8:\'\',c.9?\'; 9=\'+c.9:\'\',c.m?\'; m\':\'\'].z(\'\'))}c=b||{};3 e,o=c.i?4(s){2 s}:p;2(e=f E(\'(?:^|; )\'+7(a)+\'=([^;]*)\').F(n.5))?o(e[1]):k};4 j(b){3 c=7(b);3 d=c.G(/(%H|%I|%J|%K|%L|%M|%N)/g,4(a){2 p(a)});2 d}',51,51,'expires||return|var|function|cookie|if|encodeURIComponent|path|domain||||||new||String|raw|cookie_encode|null|jQuery|secure|document|decode|decodeURIComponent|typeof|number|||arguments|toUTCString|object|Object|extend|join|Date|setDate|length|undefined|RegExp|exec|replace|7B|7D|3A|22|23|5B|5D|getDate'.split('|'),0,{}));
